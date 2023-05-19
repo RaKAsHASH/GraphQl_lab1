54c58f2632e9eaa8943e7b94bdcdd2ff509925bf
RaKAsHASH <theonlyking135@gmail.com>
2023-05-19 17:00:57 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express= require('express')
+const expressGraphQL=require('express-graphql').graphqlHTTP
+
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString,
+}=require('graphql')
+
+const schema=new GraphQLSchema({
+    query:new GraphQLObjectType({
+            name:'HelloWorld',
+            fields:()=>({
+                message:{
+                    type:GraphQLString,
+                    resolve:()=>'Hello World'
+                }
+            })
+
+            
+    })
+})
+
+const app=express()
+app.use('/',expressGraphQL({
+    schema:schema,
+    graphiql:true,
+
+}))
+
+const PORT=8082
+app.listen(PORT,()=>console.log('Server Running'))
